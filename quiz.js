26f42ad2edc14a1c927d1ae30ef768bd116db3b6
aundykmahesh <aundykmahesh@hotmail.com>
2018-05-03 21:20:26 +1000

    just

--- quiz.js
+++ quiz.js
@@ -1,9 +1,13 @@
 
-$.getJSON( "quiz.json", function( data ) {
+$.getJSON( "https://raw.githubusercontent.com/aundykmahesh/htmlquiz/master/quiz.json", function( data ) {
     var items = [];
-    $.each( data, function( json ) {
+    $.each( data, function( json,j2 ) {
       //items.push( "<li id='" + key + "'>" + val + "</li>" );
       alert(json);
+      alert(j2.options[1].value);
+      // $.each(j2.options, function(answers){
+      //   alert(answers);
+      // });
     });
    
     // $("<ul/>", {
